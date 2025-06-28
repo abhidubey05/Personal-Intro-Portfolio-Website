55f522b9ac321b553185ef32f3557b3cfe16ab59
Abhishek Kumar Dubey <abhishekkumardubey540@gmail.com>
2025-06-28 20:58:14 +0530

    Add files via upload

---
+++ responsive website intro/main.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Data Analyst', 'Web Designer', 'Frontend Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
